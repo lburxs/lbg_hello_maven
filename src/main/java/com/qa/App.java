59767fc4ec1a5f3503dae807e3b7e3b10d6be25d
Olly <ollymale@icloud.com>
2026-02-24 14:28:30 +0000

    EDIT: Added Malcolm

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Olly"));
+        System.out.println(sayHelloToSomeone("Malcolm"));
         System.out.println(sayGoodbye());
     }
 
